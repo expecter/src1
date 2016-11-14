912d8c14cd7d22d73aef08d33222ce4883c77c34
expecter <1005426244@qq.com>
2016-11-14 23:18:04 +0800

    1
    1

--- game/component/cc/animation.lua
+++ game/component/cc/animation.lua
@@ -9,17 +9,17 @@ function M:ctor( target ,params)
 end
 function M:setData(params )
 end
-function M:initView( target )
+function M:initView(  )
 	local tlSpriteFrame = {}
-	for i=1,6 do
-		local name = string.format("s_%d",i)
+	for i=1,9 do
+		local name = string.format("img_npc_1_att1_0%d",i)
 		local spriteFrame = display.newSpriteFrame(name..".png")
         tlSpriteFrame[#tlSpriteFrame + 1] = spriteFrame
         -- target:getSprite():setSpriteFrame(spriteFrame)
 	end
 	local animation = display.newAnimation(tlSpriteFrame, 0.1)
 	local action=cc.RepeatForever:create(cc.Animate:create(animation))
-	target:getSprite():runAction(action)
+	self.target:getSprite():runAction(action)
 	-- target:getSprite():playAnimationOnce(animation)
 end
 function M:updateView( target )
--- game/config/heroSelect.lua
+++ game/config/heroSelect.lua
@@ -14,7 +14,7 @@ local M = {
 	},
 	_children = {
 		{
-		_type = "GameNode",
+			_type = "GameNode",
 			_component = {
 				{
 					_type = "cc_node",
@@ -24,6 +24,31 @@ local M = {
 				},
 				{
 					_type = "cc_DrawComponent",
+				},
+			}
+		},
+		{
+			_type = "GameNode",
+			_component = {
+				{
+					_type = "cc_node",
+					contentsize = {width = 300,height = 240},
+					AnchPos = {x = 0,y = 0},
+					pos = {x =568,y = 100},
+				},
+				{
+					_type = "cc_spriteFrame",
+					tlFrame = {
+						"uires/cards/npc_animation_1",
+					},
+				},
+				{
+					_type = "cc_sprite",
+					spriteFrameName = "img_npc_1_stanby1_01",
+				},
+				{
+					_type = "cc_animation",
+					spriteFrameName = "img_npc_1_stanby1_01",
 				},
 			}
 		},
