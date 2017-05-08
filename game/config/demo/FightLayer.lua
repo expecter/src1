3718565ed5072a9d341461d7052b311d36eb1855
expecter <1005426244@qq.com>
2017-05-08 20:52:52 +0800

    1
    1

---
+++ game/config/demo/FightLayer.lua
@@ -0,0 +1,18 @@
+--
+-- Author: Your Name
+-- Date: 2017-05-08 19:53:46
+--
+local M = {
+	_super = "GameLayer",
+	_component = {
+		{
+			_type = "cc_node",
+			contentsize = {width = 1136,height = 640},
+			AnchPos = {x = 0,y = 0},
+			pos = {x =0,y = 0},
+		},
+
+	},
+}
+
+return M
---
+++ game/config/demo/fubenLayer.lua
@@ -0,0 +1,21 @@
+--
+-- Author: Your Name
+-- Date: 2017-05-08 19:53:46
+--
+local M = {
+	_super = "GameLayer",
+	_component = {
+		{
+			_type = "cc_node",
+			contentsize = {width = 1136,height = 640},
+			AnchPos = {x = 0,y = 0},
+			pos = {x =0,y = 0},
+		},
+		-- {
+		-- 	_type = "demo_ComTest",
+		-- },
+
+	},
+}
+
+return M
