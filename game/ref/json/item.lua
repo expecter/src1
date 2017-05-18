d217da199245a1783a03b6a11353fa5cd6c80bb7
expecter <1005426244@qq.com>
2017-05-18 18:37:11 +0800

    1
    1

---
+++ game/ref/json/item.lua
@@ -0,0 +1,12 @@
+--
+-- Author: Your Name
+-- Date: 2017-05-18 16:43:21
+--
+local M = {
+	{refId = 1,name = "",material = {},use = ""},
+	{refId = 2,name = "",material = {},use = ""},
+	{refId = 3,name = "",material = {},use = ""},
+	{refId = 4,name = "",material = {2,3},use = ""},
+}
+
+return M
---
+++ game/ref/json/use.lua
@@ -0,0 +1,11 @@
+--
+-- Author: Your Name
+-- Date: 2017-05-18 16:46:43
+--
+local M = {
+	{id = 1,work = ""},
+	{id = 2,work = ""},
+	{id = 3,work = ""},
+}
+
+return M
