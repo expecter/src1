6f84b400aa39e6f7d0a20bddd63a6227ca59b533
expecter <3556426551@163.com>
2017-05-21 23:10:45 +0800

    1
    1

---
+++ game/ref/json/getway.lua
@@ -0,0 +1,9 @@
+--
+-- Author: Your Name
+-- Date: 2017-05-20 21:33:36
+--
+local M = {
+	
+}
+
+return M
---
+++ game/ref/json/taskguide.lua
@@ -0,0 +1,9 @@
+--
+-- Author: Your Name
+-- Date: 2017-05-20 21:34:09
+--
+local M = {
+	
+}
+
+return M
