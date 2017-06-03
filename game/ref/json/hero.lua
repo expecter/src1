a5ae450f789a8e76f9a240fb6e10485b68b4c7e6
expecter <3556426551@163.com>
2017-06-03 23:30:38 +0800

    1
    1

--- game/ref/json/hero.lua
+++ game/ref/json/hero.lua
@@ -1,5 +1,8 @@
 local M = {
-	{id = 1,name = "武将1",imgId = "",desc = "武将1",li = 10,zhi = 5,fang = 5,hp = 100,mp = 50,c = 1,breakskill = 1,getWay = {{},{}}},
+	{id = 1,name = "战士",imgId = "",desc = "武将1",li = 10,zhi = 5,fang = 5,hp = 100,mp = 50,c = 1,breakskill = 1,getWay = {{},{}}},
+	{id = 1,name = "法师",imgId = "",desc = "武将1",li = 10,zhi = 5,fang = 5,hp = 100,mp = 50,c = 1,breakskill = 1,getWay = {{},{}}},
+	{id = 1,name = "牧师",imgId = "",desc = "武将1",li = 10,zhi = 5,fang = 5,hp = 100,mp = 50,c = 1,breakskill = 1,getWay = {{},{}}},
+	{id = 1,name = "刺客",imgId = "",desc = "武将1",li = 10,zhi = 5,fang = 5,hp = 100,mp = 50,c = 1,breakskill = 1,getWay = {{},{}}},
 }
 
 return M
