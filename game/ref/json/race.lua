d2ff9dc1fc6c595c2dab9ccb98848c61eed99a1a
expecter <3556426551@163.com>
2017-08-01 22:25:58 +0800

    1
    1

---
+++ game/ref/json/race.lua
@@ -0,0 +1,22 @@
+--
+-- Author: Your Name
+-- Date: 2017-08-01 21:53:40
+--
+local M = {
+	{id = 0,name = "未知",skillIds = {}},
+	{id = 1,name = "人族",skillIds = {}},
+	{id = 2,name = "兽族",skillIds = {}},
+	{id = 3,name = "海族",skillIds = {}},
+	{id = 4,name = "机械族",skillIds = {}},
+	{id = 5,name = "虫族",skillIds = {}},
+	{id = 6,name = "不死族",skillIds = {}},
+	{id = 7,name = "魔族",skillIds = {}},
+	{id = 8,name = "天使族",skillIds = {}},
+	{id = 9,name = "精灵族",skillIds = {}},
+	{id = 10,name = "仙族",skillIds = {}},
+	{id = 11,name = "神族",skillIds = {}},
+	{id = 12,name = "星灵",skillIds = {}},
+	{id = 13,name = "神族",skillIds = {}},
+}
+
+return M
