97ad21705726061ff340a5d33df0a932ddd8b58e
expecter <1005426244@qq.com>
2017-10-16 21:42:41 +0800

    1
    1

--- game/data/dataBase.lua
+++ game/data/dataBase.lua
@@ -2,9 +2,24 @@
 -- Author: Your Name
 -- Date: 2017-10-06 20:21:11
 --
-local M = {
+local _t = {
 	id = 1,
 	index = 2, --记录操作次数，方便界面计算是否更新
 }
-
-return M
+t = {}
+local mt = {
+	__index = function (t,k)
+		return _t[k]
+	end,
+	__newindex = function ( t,k,v )
+		-- print(k .. "字段是不存在的，不要试图给它赋值！");
+		print("update of element",v)
+		_t[k] = v
+	end
+}
+setmetatable(t, mt)
+local t1 = t
+t.xx = "AA"
+print(t.xx)
+print(t1.xx)
+-- return M
