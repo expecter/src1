f7dae681bcd3656889e2f706b3d59bbdba671855
expecter <1005426244@qq.com>
2017-10-18 09:27:09 +0800

    Revert "Revert "1""
    This reverts commit 21df06b2e43843add33da75b3768c7edddeaca25.

--- game/config/demo/LoginLayer.lua
+++ game/config/demo/LoginLayer.lua
@@ -4,41 +4,64 @@
 --
 local M = {
 	_super = "GameNode",
-	_name = "nodeSlider",
 	_view = {
 		_type = "node",
 		contentsize = {width = 1136,height = 640},
 		AnchPos = {x = 0,y = 0},
 		pos = {x =0,y = 0},
 	},
-	_component = {
-	},
-	_children = {
-		{
-			_super = "GameNode",
-			_name = "nodeSlider",
-			_view = {
-				_type = "node",
-				contentsize = {width = 500,height = 640},
-				-- AnchPos = {x = 0.5,y = 0.5},
-				pos = {x =568,y = 320},
-			},
-			_component = {
+	_children = {			
+			{
+				_super = "GameNode",
+				_view = {
+					_type = "node",
+					contentsize = {width = 1136,height = 640},
+					AnchPos = {x = 0,y = 0},
+					pos = {x =0,y = 0},
+				},
+				_component = {
 				{
-					_type = "ui_progressTimer",
+					_type = "ui_mutiViewComponent",
+					tlData = {
+						{path = "game.config.TabNode2",params = {}},
+						{path = "game.config.TabNode3",params = {}},
+					},
+				},
+				{
+					_type = "dispatch_switchView",
+					-- btnName = "switchNode",
+					-- eventName = 
+					tlEventName = {
+						"LoginLayer_switchto",
+					},
+				},
 				},
 			},
-		},
-		{
-			_super = "GameNode",
-			_name = "nodeSlider",
-			_view = {
-				_type = "polygon",
-				contentsize = {width = 500,height = 640},
-				AnchPos = {x = 0.5,y = 0.5},
-				pos = {x =568,y = 320},
+			{
+				_name ="switchNode",
+				_super = "GameNode",
+				_view = {
+					_type = "node",
+					contentsize = {width = 1136,height = 140},
+					AnchPos = {x = 0,y = 0},
+					pos = {x =0,y = 600},
+				},
+				_component = {
+				{
+					_type = "ui_viewList",
+					object = 
+					{
+						--道具背包数据
+						_data = "ref",
+						refName = "bagindex",
+					},
+					clickedEvent = {
+						{_type = "Message",name = "LoginLayer_switchto"}
+					},
+				}
+				},
 			},
-		},
-	}
+	},
+
 }
 return M
